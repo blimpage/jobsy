811c76118da2460420d0f72a69f01be5812a31d8
Pat McKenna <pat@mckenna.rocks>
2025-03-05 15:15:45 +1100

    Handle pagination
    My initial approach for this was gonna be "look for a 'next page' link
    on the page, scrape that URL, and keep going until we get to a page that
    doesn't have a 'next page link'". Simple.
    
    However! Very annoyingly, this Greenhouse page doesn't use regular `<a>`
    links for its pagination. Instead it uses a React component that renders
    `<button>`s, which don't expose the page URLs in any way that Nokogiri
    can access. So those pagination buttons are completely useless to us!
    Gah!!!
    
    So instead, this code manually adds the `?page=` query parameter to the
    page URL, and increments it until we reach a page that doesn't have any
    job listings. It achieves the same thing, just not in the way I'd prefer
    (which is to behave like a real user browsing through the site).

--- scraper.rb
+++ scraper.rb
@@ -1,21 +1,41 @@
 require "nokogiri"
 require "open-uri"
 
-start_page_url = "https://job-boards.greenhouse.io/gitlab/"
+START_PAGE_NUMBER = 1
 
-current_page = Nokogiri::HTML(URI.open(start_page_url))
+def url_for_page(page_number)
+  "https://job-boards.greenhouse.io/gitlab/?page=#{page_number}"
+end
 
-job_post_elements = current_page.search(".job-post")
+current_page_number = START_PAGE_NUMBER
+last_page_reached = false
+jobs = []
 
-jobs = job_post_elements.map do |post_element|
-  link_element = post_element.at("a")
-  title_element, location_element = post_element.search("p")
+until last_page_reached do
+  current_page_url = url_for_page(current_page_number)
+  current_page = Nokogiri::HTML(URI.open(current_page_url))
 
-  {
-    title: title_element.inner_text.strip,
-    location: location_element.inner_text.strip,
-    url: link_element["href"],
-  }
+  job_post_elements = current_page.search(".job-post")
+
+  if job_post_elements.none?
+    last_page_reached = true
+    next
+  end
+
+  current_page_jobs = job_post_elements.map do |post_element|
+    link_element = post_element.at("a")
+    title_element, location_element = post_element.search("p")
+
+    {
+      title: title_element.inner_text.strip,
+      location: location_element.inner_text.strip,
+      url: link_element["href"],
+      page: current_page_number,
+    }
+  end
+
+  jobs += current_page_jobs
+  current_page_number += 1
 end
 
 puts jobs
